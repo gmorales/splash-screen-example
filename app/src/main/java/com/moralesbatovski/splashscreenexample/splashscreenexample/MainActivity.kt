6f625302942798c3baab5ff6b74a2ece4074fb55
gmorales <morales.batovski@gmail.com>
2018-04-11 14:43:37 -0300

    Blank project added.

---
+++ app/src/main/java/com/moralesbatovski/splashscreenexample/splashscreenexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.moralesbatovski.splashscreenexample.splashscreenexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
